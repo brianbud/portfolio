9f93b550db2c834891d76482fc563312d1c4900c
Brian Bud <brian@brianbud.com>
2022-10-13 21:54:09 -0400

    configure template path

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,11 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main></main>
+      <main>
+        <section className=''>
+
+        </section>
+      </main>
     </div>
   );
 }
