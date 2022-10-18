c7f91928e578d0e94bf6e5853c2ab61d16699ad5
Brian Bud <brian@brianbud.com>
2022-10-17 20:21:30 -0400

    resume btn

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
             <h1 className='text-xl'>Brian Buddhadasa</h1>
             <ul className='flex items-center'>
               <li><BsFillMoonStarsFill className='cursor-pointer text-2xl'/></li>
-              <li><a className='bg-cyan-500' href="#">Resume</a></li>
+              <li><a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href="#">Resume</a></li>
             </ul>
           </nav>
         </section>
