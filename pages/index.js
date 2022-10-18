6d3f2f443fec2bf07580580844b767432062f8f6
Brian Bud <brian@brianbud.com>
2022-10-17 20:25:21 -0400

    header content

--- pages/index.js
+++ pages/index.js
@@ -13,12 +13,17 @@ export default function Home() {
       <main className='bg-white px-10'>
         <section className='min-h-screen'>
           <nav className='py-10 mb-12 flex justify-between'>
-            <h1 className='text-xl font-burtons'>Brian Buddhadasa</h1>
+            <h1 className='text-xl font-burtons'>Brian Bud</h1>
             <ul className='flex items-center'>
               <li><BsFillMoonStarsFill className='cursor-pointer text-2xl'/></li>
               <li><a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href="#">Resume</a></li>
             </ul>
           </nav>
+          <div>
+            <h2>Brian Buddhadasa</h2>
+            <h3>Web Developer</h3>
+            <p>Quisquam atque totam saepe voluptas non praesentium reiciendis cumque officia quis ab magnam deleniti provident iusto, possimus cum iure repellat rem quod?</p>
+          </div>
         </section>
       </main>
     </div>
