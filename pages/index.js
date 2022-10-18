75f7367536033fb642552a2626da173ab13aece3
Brian Bud <brian@brianbud.com>
2022-10-17 20:23:49 -0400

    font family to nav

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main className='bg-white px-10'>
         <section className='min-h-screen'>
           <nav className='py-10 mb-12 flex justify-between'>
-            <h1 className='text-xl'>Brian Buddhadasa</h1>
+            <h1 className='text-xl font-burtons'>Brian Buddhadasa</h1>
             <ul className='flex items-center'>
               <li><BsFillMoonStarsFill className='cursor-pointer text-2xl'/></li>
               <li><a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href="#">Resume</a></li>
